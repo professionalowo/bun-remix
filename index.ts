36d07134cb7a1c00ba8ce2605c53b0ee1b903076
professionalowo <martin.brandl1307@gmail.com>
2024-07-20 18:19:52 +0200

    factored out package

---
+++ index.ts
@@ -0,0 +1,34 @@
+import type { ServerBuild } from "@remix-run/server-runtime";
+import { createRequestHandler } from "@remix-run/server-runtime";
+import { resolve } from "node:path";
+export type CreateRequestHandlerArgs = {
+  build:
+    | ServerBuild
+    | Promise<ServerBuild>
+    | (() => ServerBuild | Promise<ServerBuild>);
+  mode: "production" | "development";
+};
+
+export function handler({ build: b, mode }: CreateRequestHandlerArgs) {
+  return async function (request: Request) {
+    const build = await resolveBuild(b);
+    const { pathname } = new URL(request.url);
+    const filePath = resolve(
+      process.cwd(),
+      build.assetsBuildDirectory,
+      `.${pathname}`
+    );
+    const file = Bun.file(filePath);
+    if (await file.exists()) return new Response(file);
+    return createRequestHandler(build, mode)(request);
+  };
+}
+
+async function resolveBuild(
+  build: CreateRequestHandlerArgs["build"]
+): Promise<ServerBuild> {
+  if (typeof build === "function") return await build();
+  return build;
+}
+
+export { handler as createRequestHandler };
