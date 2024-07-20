e4c0494a01dc02e5b14c9b4dc4cc2f0dd30a8869
professionalowo <martin.brandl1307@gmail.com>
2024-07-20 19:01:50 +0200

    factored out resolve build since its static on runtime

--- index.ts
+++ index.ts
@@ -8,9 +8,16 @@ type CreateRequestHandlerArgs = {
   mode: "production" | "development";
 };
 
-export function handler({ build: b, mode }: CreateRequestHandlerArgs) {
+/**
+ * @param {CreateRequestHandlerArgs} the {@link CreateRequestHandlerArgs} object
+ * @returns a funtion that delegates the request to the remix server build
+ * @author Professionalowo
+ * @since 1.0.0
+ */
+export async function handler({ build: b, mode }: CreateRequestHandlerArgs) {
+  const build = await resolveBuild(b);
   return async function (request: Request) {
-    const build = await resolveBuild(b);
+    // Try to get the file from the assets build directory
     const { pathname } = new URL(request.url);
     const filePath = resolve(
       process.cwd(),
@@ -18,11 +25,18 @@ export function handler({ build: b, mode }: CreateRequestHandlerArgs) {
       `.${pathname}`
     );
     const file = Bun.file(filePath);
+    // If the file exists, return it
     if (await file.exists()) return new Response(file);
+
+    // Otherwise, delegate the request to the remix server build
     return createRequestHandler(build, mode)(request);
   };
 }
 
+/**
+ * @param build - A promise of a remix server build, a function that returns a remix server build or a remix build server
+ * @returns A promise that resolves to the remix build
+ */
 async function resolveBuild(
   build: CreateRequestHandlerArgs["build"]
 ): Promise<ServerBuild> {
